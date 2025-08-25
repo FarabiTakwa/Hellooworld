cfb919da8612ef06c496d75e4f6afe7324a462bc
Tkiwa <takwaca1920@gmail.com>
2025-08-25 23:02:46 +0100

    Ajout Spring Boot + Dockerfile + docker-compose pour tests locaux

---
+++ src/main/java/com/takwa/demo/controller.java
@@ -0,0 +1,13 @@
+package com.takwa.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
+
